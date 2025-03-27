0acf76f71708750d30179bce3ef786d77339811d
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2025-03-26 23:47:04 -0700

    makes socket.ts connect to /gameplay

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3002";
 
-export const socket = io(SOCKET_URL, {
+export const socket = io(`${SOCKET_URL}/gameplay`, {
   autoConnect: false,
   reconnection: true,
   reconnectionAttempts: 5,
