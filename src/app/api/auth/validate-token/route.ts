5726d5712a003c4f35eec0cf02af958413932bd7
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2025-04-14 11:32:03 -0700

    cleans console.logs

--- src/app/api/auth/validate-token/route.ts
+++ src/app/api/auth/validate-token/route.ts
@@ -2,6 +2,5 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   // If middleware allows this route to be called, the token is valid
-  console.log('Token validation route called');
   return NextResponse.json({ valid: true });
 } 
