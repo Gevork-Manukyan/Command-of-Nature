fe3389436dfce4647d6b7078b3689ebfc846ce49
Gevork Manukyan <68621339+Gevork-Manukyan@users.noreply.github.com>
2025-04-20 20:14:18 -0700

    moves a bunch of types from server to shared type

---
+++ src/game-state-types.ts
@@ -0,0 +1,23 @@
+import { Card, Decklist, Sage } from './card-types';
+
+export type GameListing = {
+    id: string;
+    gameName: string;
+    isPrivate: boolean;
+    numPlayersTotal: number;
+    numCurrentPlayers: number;
+}
+
+export type Player = {
+    socketId: string;
+    isReady: boolean;
+    isSetup: boolean;
+    hasChosenWarriors: boolean;
+    isGameHost: boolean;
+    sage: Sage;
+    decklist: Decklist;
+    level: number;
+    hand: Card[];
+    deck: Card[];
+    discardPile: Card[];
+} 
--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,15 @@
-import { z } from 'zod';
-import { Card, Decklist, Sage } from './card-types';
+// Game State Types
+export type { GameListing, Player } from './game-state-types';
 
-/* ----------- GAME STATE ----------- */
-export type GameListing = {
-    id: string;
-    gameName: string;
-    isPrivate: boolean;
-    numPlayersTotal: number;
-    numCurrentPlayers: number;
-}
+// Game Setup Types
+export type { OnePlayerSpaceOptions, TwoPlayerSpaceOptions, SpaceOption } from './game-setup-types';
+export { OnePlayerSpaceOptionsSchema, AllSpaceOptionsSchema, SpaceOptionsSchema } from './game-setup-types';
 
-export type Player = {
-    socketId: string;
-    isReady: boolean;
-    isSetup: boolean;
-    hasChosenWarriors: boolean;
-    isGameHost: boolean;
-    sage: Sage;
-    decklist: Decklist;
-    level: number;
-    hand: Card[];
-    deck: Card[];
-    discardPile: Card[];
-}
+// Game Events
+export * from './game-events';
 
-/* ----------- GAME SETUP ----------- */
-export const OnePlayerSpaceOptionsSchema = z.union([
-    z.literal(1),
-    z.literal(2),
-    z.literal(3),
-    z.literal(4),
-    z.literal(5),
-    z.literal(6),
-]);
+// Ability Types
+export * from './ability-types';
 
-export const AllSpaceOptionsSchema = z.union([
-z.literal(1),
-z.literal(2),
-z.literal(3),
-z.literal(4),
-z.literal(5),
-z.literal(6),
-z.literal(7),
-z.literal(8),
-z.literal(9),
-z.literal(10),
-z.literal(11),
-z.literal(12),
-]);
-  
-export const SpaceOptionsSchema = z.union([OnePlayerSpaceOptionsSchema, AllSpaceOptionsSchema]);
-
-export type OnePlayerSpaceOptions = z.infer<typeof OnePlayerSpaceOptionsSchema>;
-export type TwoPlayerSpaceOptions = z.infer<typeof AllSpaceOptionsSchema>;
-export type SpaceOption = z.infer<typeof SpaceOptionsSchema>;
-
-/* ----------- GAMEPLAY EVENTS ----------- */
-export const CreateGameEvent = "create-game";
-export const JoinGameEvent = "join-game";
-export const PlayerJoinedEvent = "player-joined";
-export const PlayerLeftEvent = "player-left";
-export const SelectSageEvent = "select-sage";
-export const SageSelectedEvent = "sage-selected";
-export const AllSagesSelectedEvent = "all-sages-selected";
-export const ToggleReadyStatusEvent = "toggle-ready-status";
-export const ReadyStatusReadyEvent = "ready-status--ready";
-export const ReadyStatusNotReadyEvent = "ready-status--not-ready";
-export const JoinTeamEvent = "join-team";
-export const TeamJoinedEvent = "team-joined";
-export const ClearTeamsEvent = "clear-teams";
-export const AllTeamsJoinedEvent = "all-teams-joined";
-export const StartGameEvent = "start-game";
-export const ChoseWarriorsEvent = "chose-warriors";
-export const PickWarriorsEvent = "pick-warriors";
-export const SwapWarriorsEvent = "swap-warriors";
-export const PlayerFinishedSetupEvent = "player-finished-setup";
-export const CancelSetupEvent = "cancel-setup";
-export const AllPlayersSetupEvent = "all-players-setup";
-export const ExitGameEvent = "exit-game";
-export const RejoinGameEvent = "rejoin-game";
-export const PlayerRejoinedEvent = "player-rejoined";
-export const LeaveGameEvent = "leave-game";
-export const GetDayBreakCardsEvent = "get-day-break-cards";
-export const ActivateDayBreakEvent = "activate-day-break";
-export const StartTurnEvent = "start-turn";
-export const WaitingTurnEvent = "waiting-turn";
-
-export const DebugEvent = "debug";
+// Card Types
+export * from './card-types';
