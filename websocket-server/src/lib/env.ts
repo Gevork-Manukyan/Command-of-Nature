eea4174630c98a9f0f0e8d51abf1bb17a735f99b
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2025-08-01 13:15:13 -0700

    fixes env.ts on websocket server

--- websocket-server/src/lib/env.ts
+++ websocket-server/src/lib/env.ts
@@ -4,7 +4,8 @@ require('dotenv').config();
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'production']).default('development'),
     PORT: z.coerce.number().default(3003),
-    DATABASE_URL: z.string().min(1),
+    MONGODB_URI: z.string().min(1),
+    MONGODB_DB: z.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
