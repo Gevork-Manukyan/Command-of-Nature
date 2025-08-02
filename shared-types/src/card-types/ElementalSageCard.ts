ddbf7c8d11b6b7bd236286cf2765832703137709
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2025-08-02 14:47:22 -0700

    creates ElementalSageCard

---
+++ shared-types/src/card-types/ElementalSageCard.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { SageSchema } from "../card-types";
+import { ElementalWarriorStarterCard, ElementalWarriorStarterCardSchema } from "./ElementalWarriorStarterCard";
+
+export const ElementalSageCardSchema = ElementalWarriorStarterCardSchema.extend({
+    sage: SageSchema
+})
+
+type ElementalSageCardType = z.infer<typeof ElementalSageCardSchema>;
+
+export class ElementalSageCard extends ElementalWarriorStarterCard {
+    sage: ElementalSageCardType["sage"];
+
+    constructor(params: ElementalSageCardType) {
+        super(params);
+        this.sage = params.sage;
+    }
+
+    static from(data: unknown): ElementalSageCard {
+        const parsed = ElementalSageCardSchema.parse(data);
+        return new ElementalSageCard(parsed);
+    }
+}
