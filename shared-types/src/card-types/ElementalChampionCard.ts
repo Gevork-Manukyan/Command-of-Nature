796b5f268c5db7d2c0a7500f3fcafcae2acea695
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2025-08-02 14:40:21 -0700

    creates ElementalChampionCard

---
+++ shared-types/src/card-types/ElementalChampionCard.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import {
+    ElementalWarriorStarterCard,
+    ElementalWarriorStarterCardSchema,
+} from "./ElementalWarriorStarterCard";
+
+export const ElementalChampionCardSchema = ElementalWarriorStarterCardSchema.extend({
+    levelRequirement: z.union([z.literal(4), z.literal(6), z.literal(8)]),
+});
+
+type ElementalChampionCardType = z.infer<typeof ElementalChampionCardSchema>;
+
+export class ElementalChampionCard extends ElementalWarriorStarterCard {
+    levelRequirement: ElementalChampionCardType["levelRequirement"];
+
+    constructor(params: ElementalChampionCardType) {
+        super(params);
+        this.levelRequirement = params.levelRequirement;
+    }
+
+    static from(data: unknown): ElementalChampionCard {
+        const parsed = ElementalChampionCardSchema.parse(data);
+        return new ElementalChampionCard(parsed);
+    }
+}
