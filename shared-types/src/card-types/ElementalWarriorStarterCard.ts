cf4568bae23a3690fc3d4b944abe79364c3d7dbc
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2025-08-02 13:53:58 -0700

    Creates ElementalWarriorStarterCard

---
+++ shared-types/src/card-types/ElementalWarriorStarterCard.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import {
+    ElementalWarriorCard,
+    ElementalWarriorCardSchema,
+} from "./ElementalWarriorCard";
+
+export const ElementalWarriorStarterCardSchema = ElementalWarriorCardSchema.extend({
+    price: z.literal(1),
+});
+
+type ElementalWarriorStarterCardType = z.infer<typeof ElementalWarriorStarterCardSchema>;
+
+export class ElementalWarriorStarterCard extends ElementalWarriorCard {
+    constructor(params: ElementalWarriorStarterCardType) {
+        super(params);
+    }
+
+    static from(data: unknown): ElementalWarriorStarterCard {
+        const parsed = ElementalWarriorStarterCardSchema.parse(data);
+        return new ElementalWarriorStarterCard(parsed);
+    }
+}
